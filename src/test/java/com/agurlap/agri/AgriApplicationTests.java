4242a1b9bf1218607a5e5ad65965467bf81e13fd
Prashanth-Ango <prashanth@angodigitaltech.com>
2025-10-18 07:40:17 +0530

    Initial commit

---
+++ src/test/java/com/agurlap/agri/AgriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agurlap.agri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
